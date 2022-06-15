2d3672456ce7c7a978fbe66327bb283e2044a79e
scharlez <stevencharlez@gmail.com>
2022-06-15 16:33:18 -0700

    javascript-intro-challenge

---
+++ conditionals-tw-sc.js
@@ -0,0 +1,27 @@
+//#1 - Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+//var cookies = chocolate
+var cookies = 4
+
+if(cookies < 100) {
+  console.log ('In Budget!')
+}
+
+if(cookies > 100) {
+  console.log ('Out of Budget!')
+}
+
+
+//#2 - Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+
+
+//#3 - Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+
+
+//#4 - Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
+
+
+//#5 - Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
