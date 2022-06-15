a175e3f91f3455965c2b4ee359386f6bf870b31f
Genemichael25 <genemichael25@yahoo.com>
2022-06-15 16:10:10 -0700

    Finished challenges

--- james-gene.js
+++ james-gene.js
@@ -28,30 +28,60 @@ var names = "James and Gene"
 console.log(names.toUpperCase())
 //10.Write the code that will log the string in all uppercase letters.
 
-console.log("")
 //11. Write the code that will log true or false for the following:
-/*
 
-Is 34 divided by 3 greater than 67 divided by 2?
-Does 5 evaluate to the same as "5"?
-Does 5 strictly equal "5"?
-Does !3 strictly equal 3?
-Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
-Does "LEARN" contain the subset "RN"?
-Does "LEARN" contain the subset "rn"?
-Does "LEARN"[0] strictly equal "l"?
-Modify the code from the previous question to return true.
-Consider the variables:
+console.log(34/3 > 67/2)
+// Is 34 divided by 3 greater than 67 divided by 2?
+
+console.log(5 == "5")
+// Does 5 evaluate to the same as "5"?
+
+console.log(5 === "5")
+// Does 5 strictly equal "5"?
+
+console.log(3 !== 3)
+// Does !3 strictly equal 3?
+
+console.log("LEARN".length === 5 && "Student".length === 7)
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+
+console.log("LEARN".length === 5 || "Student".length === 10)
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+
+console.log("LEARN".includes("RN"))
+// Does "LEARN" contain the subset "RN"?
+
+console.log("LEARN".includes("rn"))
+// Does "LEARN" contain the subset "rn"?
+
+console.log("LEARN"[0] === "l")
+// Does "LEARN"[0] strictly equal "l"?
+
+console.log("LEARN"[0] === "L")
+// Modify the code from the previous question to return true.
+
 
 var theQuestion = "life, the universe, and everything"
 var theAnswer = 42
-Write the code that will log theAnswer divided by 2.
-Write the code that will log the length of theQuestion.
-Write the code that will log the index of the character "f" in the theQuestion.
-Write the code that will log the concatenation of the two variables.
-Write the code that will log "the universe".
-Write the code that will log the character "l" from theQuestion.
-Write the code that will log whether theQuestion.length is greater than theAnswer.*/
 
-console.log("change")
+console.log(theAnswer / 2)
+// Write the code that will log theAnswer divided by 2.
+
+console.log(theQuestion.length)
+// Write the code that will log the length of theQuestion.
+
+console.log(theQuestion.indexOf("f"))
+// Write the code that will log the index of the character "f" in the theQuestion.
+
+console.log(theAnswer + theQuestion)
+// Write the code that will log the concatenation of the two variables.
+
+console.log(theQuestion.slice(6,18))
+// Write the code that will log "the universe".
+
+console.log(theQuestion[0])
+// Write the code that will log the character "l" from theQuestion.
+
+console.log(theQuestion.length > theAnswer.length )
+// Write the code that will log whether theQuestion.length is greater than theAnswer.*/
+
