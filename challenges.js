cad61f079452a58355cb2125cac2bebf98c696cb
ahmedkhafaji <four01four@gmail.com>
2022-06-15 11:47:50 -0700

    challenges file

---
+++ challenges.js
@@ -0,0 +1,25 @@
+// Challenges
+//opy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+
+//Write the code that will log the outcome of 34 added to 71.
+//Write the code that will log the outcome of 67 subtracted from 123.
+//Write the code that will log the outcome of 56 multiplied by 23.
+//Write the code that will log the outcome of 45 divided by 5.
+//Write the code that will log the outcome of 5 to the power of 7.
+//Write the code that will log the whole number remainder of 33 divided by 6.
+//Write the code that will log the length of a string containing your name.
+//Write the code that will log whether your string includes the letter "e"?
+//Write the code that will log the character at the first index of the string.
+//Write the code that will log the string in all uppercase letters.
+//Write the code that will log true or false for the following:
+
+//Is 34 divided by 3 greater than 67 divided by 2?
+//Does 5 evaluate to the same as "5"?
+//Does 5 strictly equal "5"?
+//Does !3 strictly equal 3?
+//Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+//Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+//Does "LEARN" contain the subset "RN"?
+//Does "LEARN" contain the subset "rn"?
+//Does "LEARN"[0] strictly equal "l"?
+//Modify the code from the previous question to return true.
