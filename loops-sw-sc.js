42bbba23f20e0f975838d4a2839d0aacc9b4ed1a
k0smo <sawallace7@gmail.com>
2022-06-16 16:36:04 -0700

    loops-sw-sc

---
+++ loops-sw-sc.js
@@ -0,0 +1,28 @@
+// 1. Create a for loop that logs each number from 1 - 20.
+for(let i=0; i<20; i++){
+    console.log(i)
+  }
+// 2. Create a for loop that logs every other number from 1 - 20.
+for(let i=0; i<20; i+=2){
+    console.log(i)
+  }
+// 3. Create a for loop that logs the result of each number from 1 - 20 tripled.
+for(let i=0; i<20; i+=3){
+    console.log(i)
+  }
+// 4. Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".Expected output: ODD, 2, ODD, 4, ODD, 6 ...
+ 
+for(let i=0; i<20; i++){
+    if(i % 2 === 1){
+        console.log("odd")
+    } else {
+        console.log(i)   
+    }
+  }
+// 5. Create the code that will log the largest number from the array.
+
+// 6. Create the code that will log the smallest number from the array.
+
+// 7. Create the code that will log the remainder of each number when divided by 2.
+
+// 8. Expected output: 1, 1, -1, 0, 1
