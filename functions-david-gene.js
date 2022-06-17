65bed04ad21b331a63b7fa56ec0ab1c3e48edccd
Genemichael25 <genemichael25@yahoo.com>
2022-06-17 12:05:39 -0700

    Finished challenges

--- functions-david-gene.js
+++ functions-david-gene.js
@@ -41,10 +41,66 @@ const divisibleBy = (firstNum, secondNum) => {
 console.log(divisibleBy(10, 5))
 console.log(divisibleBy(10, 4))
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+// We need to take one arguement and return a Letter
+// Conditional will use equal to or greater/less than
 
+
+const assignGrade = (percentage) => {
+    if (percentage >= 90){
+        return "A"
+    }else if (percentage >= 80){
+        return "B"
+    }else if (percentage >= 70){
+        return "C"
+    }else if (percentage <= 69){
+        return "Needs to study harder!"
+    }
+}
+
+console.log(assignGrade(54))
+console.log(assignGrade(102))
+console.log(assignGrade(88))
+console.log(assignGrade(73))
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+// We can use abstraction for the strings.
+// By using the length of the strings we can compare
 
+const sentence = " This is a test"
+const secSentence = " One of these is longer than the other"
+const thirdSentence = " Only by a mile"
+
+const isLonger = (firstString, secondString) => {
+    if (firstString.length > secondString.length){
+        return firstString
+    } else if (firstString.length < secondString.length){
+        return secondString
+    } else if (firstString.length === secondString.length){
+        return "Both strings have same amount of characters"
+    }
+}
+
+console.log(isLonger(sentence,secSentence))
+console.log(isLonger(sentence,thirdSentence))
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
 
+const greaterNum = (firstNum, secondNum) => {
+    if (firstNum > secondNum){
+        return firstNum
+    }else if (firstNum < secondNum){
+        return secondNum
+    }else if (firstNum === secondNum){
+        return `${firstNum} is equal to ${secondNum}`
+    }
+}
+
+console.log(greaterNum(84,48))
+console.log(greaterNum(84,84))
+
 // Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+const yelling = (loudNoises) => {
+    return (loudNoises.toUpperCase())
+}
+
+console.log(yelling("i am usually very quiet"))
